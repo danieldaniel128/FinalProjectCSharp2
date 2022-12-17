48a10e37552948788a752ef0bfd50fbcb8fd25cd
amit800IL <108745954+amit800IL@users.noreply.github.com>
2022-12-17 21:32:06 +0200

    Static

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿
+﻿class Program
+{
 
-bool isDanielAGod=true;
+    static int _down;
+
+    public static int Down { get => _down; set => _down = 1 * -1; }
+
+    public static void Main(string[] args)
+    {
+        bool isDanielAPeasent = true;
 
 
-if (isDanielAGod)
-    Console.WriteLine("You insects");
-else
-    Console.WriteLine("Daniel Is Super God");
+        if (isDanielAPeasent)
+            Console.WriteLine("He is an insect");
+        else
+            Console.WriteLine("nothing, he is still an insect");
+
+    }
+}
+
+
+
 
 
 
