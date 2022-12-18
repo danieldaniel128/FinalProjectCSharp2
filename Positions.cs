1d08fcda2720e428102c6f5aa5d83617dda253d7
amit800IL <108745954+amit800IL@users.noreply.github.com>
2022-12-18 11:08:34 +0200

    IPositining interface&Positions Struct

---
+++ Positions.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+struct Positions : IPositioning
+{
+    private int _x;
+    private int _y;
+    int IPositioning.X { get => _x; set => _x = value; }
+    int IPositioning.Y { get => _y; set => _y = value; }
+
+    public Positions(int x, int y)
+    {
+       this._x = x;
+       this._y = y;
+    }
+
+
+    public static Positions operator+(Positions pos1, Positions pos2)
+    {
+        Positions pos3;
+
+        pos3._x = pos1._x + pos2._x;
+        pos3._y = pos1._y + pos2._y;
+
+        return pos3;
+    }
+
+    public static Positions operator -(Positions pos1, Positions pos2)
+    {
+        Positions pos3;
+
+        pos3._x = pos1._x - pos2._x;
+        pos3._y = pos1._y - pos2._y;
+
+        return pos3;
+    }
+
+
+
+    public override string ToString() => String.Format("The Position is" + " X :" + _x + " Y: " + _y);
+    
+
+    public override bool Equals(object obj)
+    {
+        if (!(obj is Positions))
+        {
+            return false;
+        }
+        else
+        {
+            Positions positions = (Positions)obj;
+
+            if (positions.GetHashCode() == GetHashCode())
+            {
+                return true;
+            }
+            
+        }
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return _x * _y * 3 + _y;
+    }
+}
+
