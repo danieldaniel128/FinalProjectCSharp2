eba26ceacd4c87458fc7a50c89a5846ef42fc033
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-12-20 20:49:22 +0200

    ForKremer

--- VectorTesting.cs
+++ VectorTesting.cs
@@ -14,14 +14,29 @@ namespace FinalProjectCSharp2
     public  class VectorTesting
     {
 
-        static Vector vector = new Vector();
+        static Vector2Extension vector = new Vector2Extension();
         public static void Test()
         {
-          var pos = vector.CreateVector(3, 5);
+          var pos = vector.CreateVector(7, 10);
           var pos2 = vector.CreateVector(3, 5);
-          //pos += Vector.Down;
-          Console.WriteLine(pos.ToString());
-          Console.WriteLine(pos.Equals(pos2));
+          var pos3 = vector.CreateVector(3, 7);
+            pos3 += Vector2Extension.Left;
+          Vector2 pos4 = new Vector2(5,6);
+
+            //pos += Vector.Down;          //  Console.WriteLine(vector.VectorValueAsVector(pos));
+          //  Console.WriteLine(pos.ToString());
+          //Console.WriteLine(pos.Equals(pos2));
+          //  Console.WriteLine(Vector2.Distance(pos, pos2));
+          //  Console.WriteLine(Vector2.Normalize(pos));
+            while (pos != pos2)
+            {
+               
+             
+                pos =Vector2Extension.MoveTowards(pos, pos2, 0.001f);
+                Console.WriteLine("Current position: " + pos);
+
+
+            }
 
         }
 
