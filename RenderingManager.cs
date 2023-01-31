77ac5878ec2f74b9590d0ed307db54edfe77d068
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2023-01-31 21:30:14 +0200

    Added Class RenderingManager

---
+++ RenderingManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProjectCSharp2
+{
+     class RenderingManager
+    {
+        public UpgradedTileMap TileMap { get; set; }//private set or method that will set differently, maybe only private and will get access only through Tilemap???
+
+
+        public RenderingManager(UpgradedTileMap tileMap)
+        {
+            TileMap = tileMap;
+
+        }
+    }
+}
