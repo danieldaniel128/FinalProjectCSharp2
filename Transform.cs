8edf8fc18e0b2f0fa3b6fbc957a8004d0a41fdbb
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-02-09 23:44:59 +0200

    9.2.23 changes- Not Ready

--- Transform.cs
+++ Transform.cs
@@ -2,7 +2,7 @@
 
 public class Transform : Component
 {
-    public MyVector2 Position;
+    public MyVector2 Position = new MyVector2(0,0);
     // update with grid
     public Transform(TileObject gameObject) : base(gameObject)
     {
