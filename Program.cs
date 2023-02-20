0324b2ee606be6cab7d673a2cc0bd93833f689d8
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-02-20 20:30:39 +0200

    Project Orgnization
    Deleted Unneeded Classes

--- Program.cs
+++ Program.cs
@@ -7,16 +7,7 @@ class Program
 
     public static void Main(string[] args)
     {
-        TileMap.Instance.Inititialize(8, 8);
-        Tile[,] grid = TileMap.Instance.Grid;
-        GameObjectCreator<CheckerObject>.AddToGrid(0, 'f', new MyVector2(0, 0), new MyVector2(7, 1), ConsoleColor.DarkGreen);
-        GameObjectCreator<CheckerObject>.AddToGrid(1, 'T', new MyVector2(0, 6), new MyVector2(7, 7), ConsoleColor.Blue);
 
-  
-        EngineManager.Instance.renderingManager.ChangeGridToChessGrid(ConsoleColor.DarkRed);
-        EngineManager.Instance.EngineLoop();
-
-        //Need Restriction : when it's the turn of actor 0, and i go the grid of actor 1, and object will appear on actor 0's part
 
     }
 
